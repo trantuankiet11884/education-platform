2403b1c7769a076c6c3a35b28c12d685f48963b7
trantuankiet11884 <trantuankiet11884@gmail.com>
2025-04-24 11:17:11 +0700

    2 commit

--- lib/auth-provider.tsx
+++ lib/auth-provider.tsx
@@ -20,13 +20,13 @@ import { useRouter } from "next/navigation";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWCDwZIcU8vBdsSx41C82tj37WJg86wWM",
-  authDomain: "auth-2babd.firebaseapp.com",
-  databaseURL: "https://auth-2babd-default-rtdb.firebaseio.com",
-  projectId: "auth-2babd",
-  storageBucket: "auth-2babd.appspot.com",
-  messagingSenderId: "744312420718",
-  appId: "1:744312420718:web:0debd0ef143ab52886297f",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
